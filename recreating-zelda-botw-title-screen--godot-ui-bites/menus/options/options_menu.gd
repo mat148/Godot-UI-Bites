6af93312f3df86b58d84001a6a4ab0c65d0465b5
mat148 <mrm1779@gmail.com>
2025-06-04 16:34:20 -0700

    Options menu close button

--- recreating-zelda-botw-title-screen--godot-ui-bites/menus/options/options_menu.gd
+++ recreating-zelda-botw-title-screen--godot-ui-bites/menus/options/options_menu.gd
@@ -4,6 +4,7 @@ signal close_pressed
 
 @onready var options_container: VBoxContainer = %OptionsContainer
 @onready var option_description: Label = %OptionDescription
+@onready var option_close_buton: Button = %OptionsMenuCloseButton
 
 func _ready() -> void:
 	%TitleMenu.options_pressed.connect(Callable(self, "_on_options_pressed"))
@@ -24,6 +25,6 @@ func _on_option_active_changed(active: bool, option_description: String) -> void
 func _on_options_pressed() -> void:
 	visible = true
 
-func _on_close_pressed() -> void:
+func _on_options_menu_close_button_pressed() -> void:
 	visible = false
 	close_pressed.emit()
