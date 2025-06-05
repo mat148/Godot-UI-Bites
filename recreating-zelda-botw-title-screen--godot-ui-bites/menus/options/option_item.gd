e3286f4b0bfad464dbb454039f818ea7ec4622ac
mat148 <mrm1779@gmail.com>
2025-06-05 15:52:04 -0700

    Options menu updates

--- recreating-zelda-botw-title-screen--godot-ui-bites/menus/options/option_item.gd
+++ recreating-zelda-botw-title-screen--godot-ui-bites/menus/options/option_item.gd
@@ -1,9 +1,10 @@
-class_name OptionItem extends Panel
+class_name OptionItem extends PanelContainer
 
 signal option_active_changed(active: bool, option_description: String)
 
 @onready var option_label: Label = %OptionLabel
 @onready var option_container: HBoxContainer = %OptionContainer
+@onready var option_click: Panel = %OptionClick
 
 @export var option_name: String = ""
 @export var option_action: PackedScene
@@ -12,6 +13,7 @@ signal option_active_changed(active: bool, option_description: String)
 func _ready() -> void:
 	option_label.text = option_name
 	var option = option_action.instantiate()
+	option.option_clicked.connect(_on_option_clicked)
 	option_container.add_child(option)
 	option.active_changed.connect(_on_option_active_changed)
 
@@ -26,3 +28,6 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	theme_type_variation = "OptionItemDefault"
 	option_active_changed.emit(false, option_description)
+
+func _on_option_clicked() -> void:
+	option_click.get_node('AnimationPlayer').play('option_clicked')
--- recreating-zelda-botw-title-screen--godot-ui-bites/menus/options/options_toggle_button.gd
+++ recreating-zelda-botw-title-screen--godot-ui-bites/menus/options/options_toggle_button.gd
@@ -1,18 +1,26 @@
-extends Button
+extends PanelContainer
 
+signal option_clicked
 signal active_changed(active: bool)
+
+@onready var options_toggle_button: Button = %OptionToggleButton
+@onready var select_state: NinePatchRect = %SelectState
 
 @export var toggle_on_text: String = "On"
 @export var toggle_off_text: String = "Off"
 
 func _ready() -> void:
-	text = toggle_off_text
+	options_toggle_button.text = toggle_off_text
+	select_state.visible = false
 
-func _on_toggled(button_pressed: bool) -> void:
-	text = toggle_on_text if button_pressed else toggle_off_text
+func _on_option_toggle_button_toggled(button_pressed: bool) -> void:
+	options_toggle_button.text = toggle_on_text if button_pressed else toggle_off_text
+	option_clicked.emit()
 
 func _on_mouse_entered() -> void:
 	active_changed.emit(true)
+	select_state.visible = true
 
 func _on_mouse_exited() -> void:
 	active_changed.emit(false)
+	select_state.visible = false
